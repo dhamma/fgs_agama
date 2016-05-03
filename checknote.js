e487aabdebf8b5a554e4db601cf98e976c295d04
yapcheahshen <yapcheahshen@gmail.com>
2016-05-03 23:32:13 +0800

    check continuity of note num , found 4 holes

---
+++ checknote.js
@@ -0,0 +1,30 @@
+/* check if any hole in note group, and return number group*/
+var fs=require("fs");
+var lst=fs.readFileSync("xml_note/note.lst","utf8").split(/\r?\n/);
+
+var batchcount=0;
+var prev=0;
+var processfile=function(fn) {
+	var content=fs.readFileSync("xml_note/"+fn,"utf8");
+
+	content.replace(/<ndef n="(.+?)"/g,function(m,m1){
+		var n=parseInt(m1);
+		if (n==1) {
+			batchcount++;
+		} else if (n!==prev+1) {
+			console.log("hole in ",fn, " note",n);
+		}
+		prev=n;
+
+	});
+}
+lst.forEach(processfile);
+console.log("batch ",batchcount);
+/*
+hole in  1a016r.xml  note 103
+hole in  1a016r.xml  note 105
+hole in  1a032r.xml  note 101
+hole in  1d009r.xml  note 116
+
+wrong markup in html , need manual fix.
+*/
