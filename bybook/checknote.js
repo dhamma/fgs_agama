fed5d71d09e1d0c0ead27ef0617deb64b71260b6
yapcheahshen <yapcheahshen@gmail.com>
2016-06-03 22:56:49 +0800

    1a051r 112 before 123 should be 122

--- bybook/checknote.js
+++ bybook/checknote.js
@@ -1,5 +1,14 @@
 var books=["sa1","sa2","sa3","sa4","ma1","ma2","ma3","ma4","da1","da2","ea1","ea2","ea3","ea4"];
 var fs=require("fs");
+var diff=function(note,ndef){
+	var out=[];
+	for (var i=0;i<note.length;i++) {
+		if (ndef.indexOf(note[i])==-1) {
+			out.push(note[i]);
+		}
+	}
+	return out;
+}
 var processfile=function(fn){
 	var book=fs.readFileSync(fn+".xml","utf8");
 	var note=fs.readFileSync(fn+"r.xml","utf8");
@@ -14,6 +23,8 @@ var processfile=function(fn){
 
 	if (notes.length!==ndef.length){
 		console.log("note count not match",fn,notes.length,ndef.length);
+		var dd=diff(notes,ndef);
+		fs.writeFileSync(fn+"-missnote.txt",dd.join("\n"),"utf8");
 	}
 
 }
