428cead938051a74c1d085c844d652b2f6991e59
yapcheahshen <yapcheahshen@gmail.com>
2016-06-04 00:30:08 +0800

    add para start tag

--- xml/combine.js
+++ xml/combine.js
@@ -17,8 +17,8 @@ var processfile=function(fn) {
 	var content=fs.readFileSync(fn,"utf8").replace(/\r?\n/g,"\n");
 	//add <p/> for beginning of paragraph
 
-	content=content.replace(/^([^<])/g,function(m,m1){
-		return "<p/>"+m1;
+	content=content.replace(/\n([^<])/g,function(m,m1){
+		return "\n<p/>"+m1;
 	})
 
 	if (content[content.length-1]!=="\n") content+="\n";
