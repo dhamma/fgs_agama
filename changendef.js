8a84b422aab33f6d9deceec46d3bc92b0b3ab370
yapcheahshen <yapcheahshen@gmail.com>
2016-09-01 23:43:25 +0800

    all foot note and ndef align

--- changendef.js
+++ changendef.js
@@ -1,16 +1,18 @@
 /* note id reset by page number*/
 var fs=require("fs");
 var sourcepath="xml_lb/";
+var notepath="bybook/";
 var targetpath="out/";
 var lst=fs.readFileSync(sourcepath+"file.lst","utf8").split(/\r?\n/);
+var totalnotecount=0;
 var getnotefn=function(fn){
 	var notefn=fn.replace(".txt","r.xml");
 	return notefn;
 }
 var loadnote=function(fn){
 	var notefn=getnotefn(fn);
-	var content=fs.readFileSync(sourcepath+notefn,"utf8");
-	var out={};
+	var content=fs.readFileSync(notepath+notefn,"utf8");
+	var out=[];
 	content=content.replace(/<ndef n="(.*?)" star="1"\/>/g,function(m,m1){
 		return "#"+m1+"*";
 	});
@@ -22,45 +24,61 @@ var loadnote=function(fn){
 	var previd="",previdx,prevstar;
 	content.replace(/(#[demps.0-9]+)(\*?)/g,function(m,id,star,idx){
 		if (previd) {
-			out[previd]={note:content.substring(previdx,idx).trim()};
-			if (prevstar) out[previd].star=true;
+			out.push(["",content.substring(previdx,idx).trim(),prevstar?"*":""]);
 		}
 		star=star||"";
 		prevstar=star;
 		previdx=idx+id.length+star.length;
 		previd= id.substr(1);
 	})
-	out[previd]={note:content.substr(previd)};
-	if (prevstar) out[previd].star=true;
+	out.push(["",content.substring(previdx).trim(),prevstar?"*":""]);
 
 	return out;
 }
 var processfile=function(fn){
-//	var note=loadnote(fn);
+	var note=loadnote(fn);
 
 	var content=fs.readFileSync(sourcepath+fn,"utf8");
 	var agama=fn.substr(0,1);
-	var evenpage=0,seq=0,prevoldseq=0,notecount=0,prevgroup;
+	var oddpage=0,prevoddpage=0,seq=0,prevoldseq=0,notecount=0,prevgroup;
 	content=content.replace(/([~#])([ademps.0-9]+)/g,function(m,type,id){
 		if (type=="#") {
-			seq++;
-			notecount++;
 			var at=id.indexOf(".");
 			var group=id.substr(1,at-1);
 			var oldseq=parseInt(id.substr(at+1));
 
-			if (group!=prevgroup && (oldseq!==1 && oldseq!==prevoldseq+1)){
+			if (group!=prevgroup && (oldseq!==1 && oldseq!==prevoldseq+1)){ //GAP CROSSING GROUP
 				console.log(fn,id,"note seq problem,group",group,"previous group",prevgroup,"prev seq",prevoldseq,"seq",oldseq);
 			}
+			if (group==prevgroup && oldseq<prevoldseq){ //GAP IN SAME GROUP
+				console.log(fn,id,"note seq problem,group",group,"previous group",prevgroup,"prev seq",prevoldseq,"seq",oldseq);
+			}
+			if (group==prevgroup && oldseq!==prevoldseq+1){ //GAP IN SAME GROUP
+				console.log(fn,id,"note seq problem,group",group,"previous group",prevgroup,"prev seq",prevoldseq,"seq",oldseq);
+			}			
 			prevgroup=group;
 			prevoldseq=oldseq;
-			var newid=evenpage+"."+seq;
+			seq++;
+			var newid=oddpage+"."+seq;
+			if (!note[notecount]){
+				console.log("too many note",notecount,fn)
+			} else {
+				note[notecount][0]=type+newid;	
+			}
+			
+			notecount++;totalnotecount++;
+
 			return type+newid;
 		} else {
 			id=parseInt(id);
-			if (id%2==1) id--;
-			evenpage=agama+id; //use even page number to group footnote
-			seq=0;
+			previd=id;
+			if (id%2==0) id++;
+			oddpage=agama+id; //use even page number to group footnote
+			if (prevoddpage!==oddpage){
+				seq=0;
+			}
+			prevoddpage=oddpage;
+			/*TODO workaround for DA1 preface*/
 			return type+agama+id;
 		}
 	});
@@ -69,13 +87,18 @@ var processfile=function(fn){
 			return "^"+agama+m1;
 		});		
 	}
+
+	if (notecount!=note.length) {
+		console.log("Remaining",note.length-notecount,"foot note",fn)
+	}
 	console.log("write to "+targetpath+fn,"note count",notecount);
 	fs.writeFileSync(targetpath+fn,content,"utf8");
 
-/*
+
 	var notefn=getnotefn(fn);
+	note=note.map(function(n){return n.join("\t")})
 	fs.writeFileSync(targetpath+notefn.replace(".xml",".json")
 		,JSON.stringify(note,""," "),"utf8");	
-*/
 }
 lst.forEach(processfile);
+console.log("notecount",totalnotecount);
--- checknote.js
+++ checknote.js
@@ -1,25 +1,29 @@
 /* check if any hole in note group, and return number group*/
 var fs=require("fs");
-var lst=fs.readFileSync("xml_note/note.lst","utf8").split(/\r?\n/);
+var lst=fs.readFileSync("xml_lb/file.lst","utf8").split(/\r?\n/);
 
-var batchcount=0;
+var batchcount=0,totalnotecount=0,totalndefcount=0;
 var prev=0;
 var processfile=function(fn) {
-	var content=fs.readFileSync("xml_note/"+fn,"utf8");
+	var notefn=fn.replace(".txt","r.xml");
 
-	content.replace(/<ndef n="(.+?)"/g,function(m,m1){
-		var n=parseInt(m1);
-		if (n==1) {
-			batchcount++;
-		} else if (n!==prev+1) {
-			console.log("hole in ",fn, " note",n);
-		}
-		prev=n;
-
+	var notecount=0,ndefcount=0;
+	
+	var content=fs.readFileSync("xml_lb/"+fn,"utf8");
+	content.replace(/#[aepsmnd0-9.]+/g,function(m,m1){
+		notecount++;totalnotecount++;
 	});
+
+	notecontent=fs.readFileSync("bybook/"+notefn,"utf8");
+	notecontent.replace(/<ndef n="(.+?)"/g,function(m,m1){
+		ndefcount++;totalndefcount++;
+	});
+
+	console.log(fn,"footnote",notecount,"ndef",ndefcount,ndefcount-notecount);
+
 }
 lst.forEach(processfile);
-console.log("batch ",batchcount);
+console.log("total note",totalnotecount,"total ndef",totalndefcount);
 /*
 hole in  1a016r.xml  note 103
 hole in  1a016r.xml  note 105
@@ -27,4 +31,4 @@ hole in  1a032r.xml  note 101
 hole in  1d009r.xml  note 116
 
 wrong markup in html, fixed with gen_note.js fixHoles_2016_5_5
-*
+*/
